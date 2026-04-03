593d2f8508b21955e455d16be2e21f42d8354915
demurean <arielle.felicia@gmail.com>
2026-04-03 04:14:22 -0700

    client-server only tested with one client so far

--- client.py
+++ client.py
@@ -5,8 +5,9 @@ import sys
 
 SERVER_HOST = '127.0.0.1'
 SERVER_PORT = 5050
+Username = None
 
-## 1. CLIENT ESTABLISHING CONNECTION TO SERVER
+## 1. CLIENT ESTABLISHING CONNECTION TO SERVER + Choosing Username
 def lobby(s):
     s.sendall(b'LOBBY')
     data = s.recv(1024).decode().strip()
@@ -15,11 +16,25 @@ def lobby(s):
     if parts[0] == "USERNAMES":
         UsernameList = parts[1:]
         print('Received', UsernameList)
-        ChosenUsername = input("Choose a username from the list: ")
-        message = f"REGISTER {ChosenUsername}"
+        Username = input("Choose a username from the list: ")
+        # no input error protection since this is going to be drop down list
+        message = f"REGISTER {Username}"
         s.sendall(message.encode())
         response = s.recv(1024).decode().strip()
-        print("Server:", response) 
+        print("Server:", response)
+
+def channel_lobby(s):
+    print("A. Channel 1 \nB. Channel 2")
+    answer = input("Select Channel letter to join in(A or B) ")
+    # ^^ since we're going to be GUI, no edge case protection here
+    if answer == "A":
+        Channel = "Channel1"
+    elif answer == "B":
+        Channel = "Channel2"
+    message = f"JOIN {Channel}"
+    s.sendall(message.encode())
+    response = s.recv(1024).decode().strip()
+    print("Server:", response)
 
 def start_app():
     print("starting the application...")
@@ -27,14 +42,8 @@ def start_app():
     # SERVER_PORT = 5050
     SERVER_SOCKET = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     SERVER_SOCKET.connect((SERVER_HOST,SERVER_PORT))
-    while True:
-        try: # using this structure so client socket doesn't close in waiting for server
-            lobby(SERVER_SOCKET)
-
-        except Exception as e:
-            print("Client error:", e)
-            break
-        
+    lobby(SERVER_SOCKET)
+    channel_lobby(SERVER_SOCKET)
 
 start_app()
 # order of execution:
